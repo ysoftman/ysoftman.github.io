a9fe9f92e5af20172b345cbd266375d4ade610e6
ysoftman <ysoftman@gmail.com>
2016-11-29 19:24:21 +0900

    yum -y 옵션 추가

--- installvimgo.sh
+++ installvimgo.sh
@@ -15,9 +15,9 @@ elif [ $(uname) == 'Linux' ]; then
 	# yum 실행보기 
 	yum --version
 	# yum 실행후 exit code 0(SUCCESS) 이라면 사용할수 있다.
-	package_program="yum"
+	package_program=""
 	if [ $? == 0 ]; then
-		package_program="yum"
+		package_program="yum -y"
 	else
 		package_program="apt-get"
 	fi
