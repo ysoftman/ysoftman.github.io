bb893ab7605b7adbb9f3e80c475aca0b4ae38671
ysoftman <ysoftman@gmail.com>
2016-11-30 15:24:59 +0900

    리스트 수집 커맨드 수정

--- installbypip.sh
+++ installbypip.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # 참고, pip 리스트 수집시
-# pip list | awk '{print $1}' > installbypip.sh
+# pip list | awk '{print $1}' | tr "\n" " "
 
 pip install backports-abc backports.ssl-match-hostname beautifulsoup4 blessings bs4 certifi colorama colour-runner cycler Django funcsigs green http httpclient Jinja2 List MarkupSafe matplotlib mjson mock nose numpy pbr Pillow pip publicsuffix Pygments pylib PyMySQL pyparsing python-dateutil python-termstyle pytz requests setuptools sh singledispatch six tornado Unidecode url vboxapi wheel 
