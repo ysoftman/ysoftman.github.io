19eafcea025b4f894fb2ce25a36884100b89f8e6
ysoftman <ysoftman@gmail.com>
2023-06-28 19:05:29 +0900

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-new Promise((function(o,n){o("success")})).then((function(){$("#navigation").load("navbar.html",(function(o,n,t){console.log("navbar.html loaded,status:",n)}))})).then((function(){"programlist"==window.location.search.substring(1)?$.get("/program_list.html",(function(o,n){$("#main_view").html(o),console.log("program_list loaded")})):$.get("/about_me.md",(function(o,n){let t=marked(o);$("#main_view").html(t),console.log("about_me loaded")}))})),$(document).ready((function(){console.log("document ready")}));
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/***/ (() => {
+
+eval("// showdownjs 은 리스트 하위 항목 들여쓰기 기능이 동작하지 않는다.\n// https://github.com/showdownjs/showdown/issues/615\n// let converter = new showdown.Converter();\n// converter.disableForced4SpacesIndentedSublists = true;\n\nlet pt = new Promise(function (success, fail) {\n    success(\"success\")\n});\n\npt.then(function () {\n    $(\"#navigation\").load(\"navbar.html\", function (response, status, xhr) {\n        console.log(\"navbar.html loaded,status:\", status);\n    })\n}).then(function () {\n    let param = window.location.search.substring(1);\n    // console.log('param:', param);\n    if (param == \"programlist\") {\n        // $(\"#main_view\").load(\"/program_list.html\", function () { console.log(\"program_list loaded\") });\n        $.get(\"/program_list.html\", function (data, status) {\n            $(\"#main_view\").html(data);\n            console.log(\"program_list loaded\")\n        });\n    } else {\n        $.get(\"/about_me.md\", function (data, status) {\n            // showdown 사용할때\n            // let html = converter.makeHtml(data);\n            // marked 사용할때\n            let html = marked(data);\n            //document.getElementById('main_view').innerHTML = html;\n            $(\"#main_view\").html(html);\n            console.log(\"about_me loaded\")\n        });\n    }\n});\n\n$(document).ready(function () {\n    console.log(\"document ready\");\n});\n\n\n//# sourceURL=webpack://ysoftman.github.io/./index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./index.js"]();
+/******/ 	
+/******/ })()
+;
