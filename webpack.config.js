3c102237c3cf967952522cba3ef734df79dee7ea
ysoftman <ysoftman@gmail.com>
2023-06-28 19:06:09 +0900

    add mode filed in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 module.exports = {
     target: 'node',
     entry: './index.js',
+    mode: 'development',
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, 'dist'),
