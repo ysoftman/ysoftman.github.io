ad9d03a6e1fc862d6ae073268e85504c0f6051b8
ysoftman <ysoftman@gmail.com>
2024-05-05 11:00:17 +0900

    prettier formatting

--- server.js
+++ server.js
@@ -8,8 +8,10 @@
 // })
 
 // static 리소스 서빙
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('listening on http://127.0.0.1:8080/dist');
-});
+var connect = require("connect");
+var serveStatic = require("serve-static");
+connect()
+  .use(serveStatic(__dirname))
+  .listen(8080, function () {
+    console.log("listening on http://127.0.0.1:8080/dist");
+  });
