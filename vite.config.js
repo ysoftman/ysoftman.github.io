2d96be3ff09fc61b31671c0cff02d9777ea7759b
ysoftman <ysoftman@gmail.com>
2024-12-01 13:04:13 +0900

    fix base to / and add --base options in local test

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   root: process.cwd() + "/src",
   entry: "./src/index.js",
   mode: "development",
-  base: "/dist/",
+  base: "/",
   server: {
     host: "127.0.0.1",
     port: 8080,
