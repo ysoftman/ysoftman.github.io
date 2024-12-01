dc707a7d080760fe6e042f884f59df0ad7552978
ysoftman <ysoftman@gmail.com>
2024-12-02 00:52:02 +0900

    add build timestamp and page version

--- src/pageinfo.js
+++ src/pageinfo.js
@@ -4,7 +4,14 @@ import packageJSON from "../package.json" assert { type: "json" };
 //innerHTML 로 dom 변경은 window.onload 로 보장할 수 없다.
 //window.onload = function () {
 export const pageinfoAddEventListener = function () {
-  let html = "";
+  let html = "<br>";
+  html += "[env from vite.config.js (vite 로 빌드시)]";
+  html += "<br>";
+  html += "page version: " + __PAGE_VERSION__;
+  html += "<br>";
+  html += "build timestamp: " + __BUILD_TIMESTAMP__;
+  html += "<br>";
+  html += "<br>";
   html += "[package.json]";
   html += "<br>";
   html += JSON.stringify(packageJSON, null, 2);
