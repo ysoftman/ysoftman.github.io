e7f641421d30cefe372e5a3b8eaedfa52f3db54f
ysoftman <ysoftman@gmail.com>
2024-12-20 13:12:50 +0900

    set timezone to asia/seoul

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,10 @@ export default defineConfig({
 
   define: {
     __PAGE_VERSION__: JSON.stringify("v0.1.0"),
-    __BUILD_TIMESTAMP__: "'" + new Date().toLocaleString("ko-kr") + "'",
+    __BUILD_TIMESTAMP__:
+      "'" +
+      new Date().toLocaleString("ko-kr", { timezone: "Asia/Seoul" }) +
+      "'",
     __MYENV_READONLY_TOKEN__: "'" + process.env.myenv_readonly_token + "'",
   },
 
