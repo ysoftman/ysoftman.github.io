e28e8b5749055d2a435581842adcda79c93ddf05
ysoftman <ysoftman@gmail.com>
2026-07-10 17:51:30 +0900

    update restaurant_list

--- src/restaurant_list.js
+++ src/restaurant_list.js
@@ -1,5 +1,10 @@
 export const restaurantlist = [
   {
+    name: "이가네양꼬치 판교본점",
+    tags: "양꼬치, 양갈비, 꿔바로, 가지볶음",
+    review: "https://maps.app.goo.gl/XkXS851wf52py1zAA",
+  },
+  {
     name: "판교 HAPA",
     tags: "짜장면,짬뽕,탕수육,중화요리",
     review: "https://maps.app.goo.gl/N1qhxRdaGFQq7qGn9",
